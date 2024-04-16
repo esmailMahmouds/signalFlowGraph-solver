7e76c21b7157062145ee80f92b6b5ff08df34013
mahfouz72 <mohamadmahfouz39@gmail.com>
2024-04-16 20:59:29 +0200

    build graph and count forward paths

--- backend/src/main/java/com/backend/BackendApplication.java
+++ backend/src/main/java/com/backend/BackendApplication.java
@@ -1,13 +1,78 @@
 package com.backend;
 
+import model.SignalFlowGraph;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 @SpringBootApplication
 public class BackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
+        Scanner in = new Scanner(System.in);
+        List<ArrayList<String>> list = new ArrayList<>();
+
+        ArrayList<String> innerList = new ArrayList<>();
+        innerList.add("R");
+        innerList.add("1");
+        innerList.add("2");
+        list.add(innerList);
+        innerList = new ArrayList<>();
+        innerList.add("1");
+        innerList.add("2");
+        innerList.add("11");
+        list.add(innerList);
+        innerList = new ArrayList<>();
+        innerList.add("2");
+        innerList.add("1");
+        innerList.add("1");
+        list.add(innerList);
+        innerList = new ArrayList<>();
+        innerList.add("2");
+        innerList.add("3");
+        innerList.add("6");
+        list.add(innerList);
+        innerList = new ArrayList<>();
+        innerList.add("3");
+        innerList.add("4");
+        innerList.add("7");
+        list.add(innerList);
+        innerList = new ArrayList<>();
+        innerList.add("4");
+        innerList.add("3");
+        innerList.add("3");
+        list.add(innerList);
+        innerList = new ArrayList<>();
+        innerList.add("4");
+        innerList.add("5");
+        innerList.add("1");
+        list.add(innerList);
+        innerList = new ArrayList<>();
+        innerList.add("4");
+        innerList.add("5");
+        innerList.add("5");
+        list.add(innerList);
+        innerList = new ArrayList<>();
+        innerList.add("5");
+        innerList.add("3");
+        innerList.add("4");
+        list.add(innerList);
+        innerList = new ArrayList<>();
+        innerList.add("5");
+        innerList.add("C");
+        innerList.add("9");
+        list.add(innerList);
+
+
+        SignalFlowGraph signalFlowGraph = new SignalFlowGraph(7, list);
+        signalFlowGraph.printGraph();
+
+        List<List<String>> forwardPaths = signalFlowGraph.getForwardPaths();
+        System.out.println("Forward Paths:" + forwardPaths);
     }
 
 }
---
+++ backend/src/main/java/model/ForwardPathsUtil.java
@@ -0,0 +1,50 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ForwardPathsUtil {
+    private List<List<String>> forwardPaths;
+    private Map<Node, List<Edge>> graph;
+    private Map<String,Boolean> visited;
+
+    public ForwardPathsUtil(Map<Node, List<Edge>> graph) {
+        this.graph = graph;
+        this.visited = new HashMap<>();
+        forwardPaths = new ArrayList<>();
+    }
+
+    public List<List<String>>  getForwardPaths() {
+        dfs(new Node("R"), new StringBuilder(), 1.0);
+        return forwardPaths;
+    }
+
+    private void dfs(Node node, StringBuilder path, double pathGain) {
+        if (node.getId().equals("C")) {
+            path.append(node.getId());
+            List<String> list = new ArrayList<>();
+            list.add(path.toString());
+            list.add(String.valueOf(pathGain));
+            forwardPaths.add(list);
+            path.deleteCharAt(path.length() - 1);
+            return;
+        }
+
+        String nodeId = node.getId();
+        visited.put(nodeId, true);
+        path.append(node.getId());
+
+        for (Edge edge : graph.get(node)) {
+            Node destination = edge.getDestination();
+            String destinationId = destination.getId();
+            if (!visited.getOrDefault(destinationId,false)){
+                dfs(destination, path, pathGain * edge.getGain());
+            }
+        }
+        path.deleteCharAt(path.length() - 1);
+        visited.put(nodeId, false);
+    }
+}
+

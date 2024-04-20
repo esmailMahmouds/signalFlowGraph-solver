20125c868c96c294cee2c955e0f2ab52ee838705
Osama-Hosny <Osama.hosny1642@gmail.com>
2024-04-20 14:22:23 +0200

    graph lib

--- frontend/src/components/SignalFlow.vue
+++ frontend/src/components/SignalFlow.vue
@@ -1,118 +1,149 @@
 <template>
-    <div class="contain">
       
 
       <div class="buttonsbar">
         <div class="machine">
-          <button @click="addMachine">N+</button>
+          <Button @click="addMachine" style="font-weight: 400;">N+</Button>
           <p>Add Node</p>
         </div>
         <div style="display: flex; flex-direction: row;">
-          <p style="font-size: 25px;">No. of nodes: </p>
-          <InputNumber style="width: 150px; height: 40px; position: relative; margin-top: 18px; margin-left: 5px;" v-model="numnodes" inputId="minmax" :min="2" :max="200" />
+          <p style="font-size: 25px; font-weight: 400;">No. of nodes: </p>
+          <InputNumber style="width: 150px; height: 40px; position: relative; margin-top: 18px; margin-left: 5px;" v-model="numnodes" inputId="minmax" :min="2" :max="200"  />
         </div>
+        
         <div style="display: flex; flex-direction: row;">
-          <p style="font-size: 25px; margin-left: -200px;">No. of numeric branches gain: </p>
-          <InputNumber style="width: 150px; height: 40px; position: relative; margin-top: 18px; margin-left: 5px;" v-model="gains" inputId="minmax" :min="2" :max="200" />
+          <Button @click="initialize" style="width: 150px; margin-left: -50px; font-weight: 400;">Save Values</Button>
+          <Button @click="showmodal=true" style="width: 120px; margin-left: 200px; font-weight:400; text-align: center; vertical-align:middle;" >Connect</Button>
         </div>
-        <div style="display: flex; flex-direction: row;">
-          <button @click="initialize" style="width: 150px; margin-left: -50px;">Save Values</button>
-          <button @click="showmodal=true" style="width: 120px; margin-left: 200px">Connect</button>
-        </div>
-        <button @click="clear" class="pi pi-trash"></button>
+        <Button @click="clear" class="pi pi-trash" style="font-weight: 400; align-content:center;"></Button>
   
         <div class="sim-controls">
-          <button @click="simulate" style="margin-right: 10px;" class="pi pi-play"></button>
+          <Button @click="simulate" style="margin-right: 10px; font-weight: 400;" class="pi pi-play"></Button>
           <!-- <button style="margin-right: 10px;" class="pi pi-stop"></button> -->
         </div>
       </div>
       <div class="modal"  v-if="showmodal">
         <div style="display: flex; flex-direction: row; margin-top: 25px; margin-left: 35px;">
-        <p style="font-size: 25px; ">Enter branch gain: </p>
-        <InputNumber style=" font-size:larger; width: 150px; height: 40px; position: relative; margin-top: 18px; margin-left: 5px;" v-model="gainamount" inputId="minmax" :min="-200" :max="200" /> 
-        <button @click="connect" style="margin-left: 75px;">Ok</button>
+          <p style="font-size: 25px; ">Enter branch gain: </p>
+          <InputNumber style=" font-size:larger; width: 150px; height: 40px; position: relative; margin-top: 18px; margin-left: 5px;" v-model="gainamount" inputId="minmax" :min="-200" :max="200" /> 
+          <button @click="connect" style="margin-left: 75px;">Ok</button>
+        </div>
+        </div>
+        <!-- <div class="overlay" v-if="showmodal"></div>
+        <div class="modal" style="width: 1900px; height: 1100px; background-color: cyan;">
+          <div style="display: flex; flex-direction: row; margin-top: 25px; margin-left: 35px;">
+            <p style="font-size: 25px; ">Enter branch gain: </p>
+            <InputNumber style=" font-size:larger; width: 150px; height: 40px; position: relative; margin-top: 18px; margin-left: 5px;" v-model="gainamount" inputId="minmax" :min="-200" :max="200" /> 
+            <button @click="connect" style="margin-left: 75px;">Ok</button>
+          </div> -->
+          <!-- </div>  -->
+          
+          
+          
+          
+          <div style="height: 800px; width: 100%; border: 2px solid black; border-radius: 10px;"> 
+            <v-network-graph
+            :nodes="nodes"
+            :edges="edges" 
+            :configs="configures"
+            v-model:selectedEdges="selectedEdges"
+            v-model:selectedNodes="selectedNodes"
+            edge-label="edges"
+            >
+            <template #edge-label="{ edge, ...slotProps }">
+              <v-edge-label :text="edge.label" align="center" vertical-align="above" v-bind="slotProps" >
+              </v-edge-label>
+              
+            </template>
+          </v-network-graph>
       </div>
-      </div>
-      <div class="overlay" v-if="showmodal"></div>
-      <div class="modal" style="width: 1900px; height: 1100px; background-color: cyan;">
-        <!-- <div style="display: flex; flex-direction: row; margin-top: 25px; margin-left: 35px;">
-        <p style="font-size: 25px; ">Enter branch gain: </p>
-        <InputNumber style=" font-size:larger; width: 150px; height: 40px; position: relative; margin-top: 18px; margin-left: 5px;" v-model="gainamount" inputId="minmax" :min="-200" :max="200" /> 
-        <button @click="connect" style="margin-left: 75px;">Ok</button>
-      </div> -->
-    </div>
-      
-
-    </div>
-    
-    <div class="canvas">
-         <div ref="container" class="display-screen"></div>     
-    </div>
   </template>
   
   <script>
-  import Konva from 'konva'
   import InputNumber from 'primevue/inputnumber';
   import axios from 'axios'
-  // import { Stage } from 'konva/lib/Stage';
-  
+  import {VNetworkGraph } from "v-network-graph"
+  import {defineConfigs } from "v-network-graph"
+  import Button from 'primevue/button'; 
   export default {
     components:{
+      Button,
       InputNumber,
+      VNetworkGraph,
       
     },
   
   data(){
   
     return{
-      tr: new Konva.Transformer({
-        borderStrokeWidth: 7, // Set the border width
-        }),
+  
+      selectedNodes: [],
+      selectedEdges: [],
       nodes:[],
       selected:[],
       adjacency:[],
       linelist:[],
+      edges:[],
       numnodes:2,
       gains:1,
       gainamount:1,
       showmodal:false,
+     configures :defineConfigs({
+    node: {
+    directionAutoAdjustment: true,
+    selectable: 2,
+    normal:{
+      type: "circle",
+      radius:20,
+    },
+    label:{
+      visible:true,
+      position:'center',
+      fontSize: 20,
+    },
+    }, // up to 2 nodes
+    
+    
+    
+    edge: {
+    gap:70,
+    marker: {
+      target: { type: "arrow" },
+    },
+    type:'curve',
+    selectable: true,
+    normal: {
+      width: 3,
+      radius:100,
+    },
+    selfLoop: {
+      radius: 14,
+      offset: 16,
+      angle: 0,
+      isClockwise: true,
+    },
+    label:{
+      visible:true,
+      position:'center',
+      fontSize: 50,
+    },
+  },
+}),
   
     };
   },
   
-  mounted() {
-       
-       const screenWidth = 2940;
-       const screenHeight = 850;
-   
-       this.stage = new Konva.Stage({
-         container: this.$refs.container,
-         width: screenWidth,
-         height: screenHeight,
-       });
-   
-      //  this.stage.on('dragend', (event) => {
-      //  const shape = event.target;
-      //  });
-       this.layer = new Konva.Layer();
-       this.stage.add(this.layer);
-       this.layer.add(this.tr);
-       this.tr.enabledAnchors([]);
-       this.tr.rotateEnabled(false); // Disable rotation
-      this.tr.anchorCornerRadius(0);
-  
-  
-      // Set up an interval to call the fetchData function every 100 milliseconds
-      // this.intervalId = setInterval(this.fetchData, 3000);
-       
-       
+  mounted() {    
+      //  const screenWidth = 2940;
+      //  const screenHeight = 850;   
        
      }, 
      methods:{
   
-      clear()
+    
+    clear()
       {
-        //write here the api responsible for clearing everything in the backend 
+        //write ere the api responsible for clearing everything in the backend 
   
         axios.delete("http://localhost:8081/clear");
   
@@ -138,143 +169,12 @@
         this.selected=[];
         this.tr.nodes([]);
         
-  
       },
-  
-  
-  //     fetchData() {
-  //       const base = this.url+this.mode;
-  //       console.log(base);
-  //       axios.get(base).then(response => {
-    
-  //   console.log(response.data);  
-  //   let todisplay = response.data;
-  //   for(const iterator in todisplay)
-  //   {
-  //     if (todisplay[iterator].id.startsWith('Q')) {
-  //     // For 'Q' items
-  //     const queueId = todisplay[iterator].id;  // "Q0", "Q1", ...
-  //     const numericPart = queueId.slice(1);
-  //     const numberr = parseInt(numericPart, 10);
-  //     const numberOfElements = todisplay[iterator].numberOfElements;
-  //     this.queues[numberr].children[2].text(numberOfElements)
-  
-  //     // Use the data for queues
-  //     // console.log(`Queue ID: ${queueId}, Number of Elements: ${numberOfElements}`);
-  //   } else if (todisplay[iterator].id.startsWith('M')) {
-  //     // For 'M' items
-  //     const machineId = todisplay[iterator].id;  // "M1", "M2", ...
-  //     const numericPart = machineId.slice(1);
-  //     const numberr = parseInt(numericPart, 10);
-  //     // const numberOfElements = todisplay[iterator].numberOfElements;
-  //     const colorr = todisplay[iterator].colour;
-  //     const red = parseInt(colorr[0], 10);
-  //     const green = parseInt(colorr[1], 10);
-  //     const blue = parseInt(colorr[2], 10);
-  
-  //     // Create an RGB color string
-  //     const color = `rgb(${red}, ${green}, ${blue})`;
-  //     this.machines[numberr].children[0].fill('#008000');
-  
-  //     this.machines[numberr].children[0].fill(color);
-  
+      handleNodeClick(){
+        console.log("Node clicked");
+      },
       
-  
-  //     // Use the data for machines
-  //     // console.log(`Machine ID: ${machineId}, Number of Elements: ${numberOfElements}`);
-  //   }
-  
-  
-  //   }   
-    
-  //   // Assume 'jsonResponse' is the JSON response you received
-  
-  // // Filter and process the data
-  
-  
-  //     })
-  //     .catch(error => {
-  //       console.error('There was an error!', error);
-        
-  //     })
-  //       // console.log('Fetching data...');
-  //     },
-  
-  
-      startFetchData1() {
-        // Clear the interval for fetchData2 if it's running
-        clearInterval(this.fetchData2Interval);
-  
-        // Set up an interval for fetchData1
-        this.fetchData1Interval = setInterval(this.fetchData1, 3000);
-      },
-      startFetchData2() {
-        // Clear the interval for fetchData1 if it's running
-        clearInterval(this.fetchData1Interval);
-  
-        // Set up an interval for fetchData2
-        this.fetchData2Interval = setInterval(this.fetchData2, 3000);
-      },
-  
-      fetchData1() {
-        // console.log('Fetching data 1...');
-        // Your fetchData1 logic here
-        axios.get('http://localhost:8081/live').then(response => {
-    
-    console.log(response.data);  
-    let todisplay = response.data;
-    for(const iterator in todisplay)
-    {
-      if (todisplay[iterator].id.startsWith('Q')) {
-      // For 'Q' items
-      const queueId = todisplay[iterator].id;  // "Q0", "Q1", ...
-      const numericPart = queueId.slice(1);
-      const numberr = parseInt(numericPart, 10);
-      const numberOfElements = todisplay[iterator].numberOfElements;
-      this.queues[numberr].children[2].text(numberOfElements)
-  
-      // Use the data for queues
-      // console.log(`Queue ID: ${queueId}, Number of Elements: ${numberOfElements}`);
-    } else if (todisplay[iterator].id.startsWith('M')) {
-      // For 'M' items
-      const machineId = todisplay[iterator].id;  // "M1", "M2", ...
-      const numericPart = machineId.slice(1);
-      const numberr = parseInt(numericPart, 10);
-      // const numberOfElements = todisplay[iterator].numberOfElements;
-      const colorr = todisplay[iterator].colour;
-      const red = parseInt(colorr[0], 10);
-      const green = parseInt(colorr[1], 10);
-      const blue = parseInt(colorr[2], 10);
-  
-      // Create an RGB color string
-      const color = `rgb(${red}, ${green}, ${blue})`;
-      this.machines[numberr].children[0].fill('#008000');
-  
-      this.machines[numberr].children[0].fill(color);
-  
-      
-  
-      // Use the data for machines
-      // console.log(`Machine ID: ${machineId}, Number of Elements: ${numberOfElements}`);
-    }
-  
-  
-    }   
-    
-    // Assume 'jsonResponse' is the JSON response you received
-  
-  // Filter and process the data
-  
-  
-      })
-      .catch(error => {
-        console.error('There was an error!', error);
-        
-      })
-        // console.log('Fetching data...');
-      },
-  
-  
+
   
       
   
@@ -283,204 +183,56 @@
         const body={architecture:this.adjacency}
         axios.post(`http://localhost:8081/construct/${this.number}`,body);
         this.startFetchData1();
+        
   
   
   //
       },
   
-  
-  
-  
-  
-      fetchData2() {
-        //  console.log('Fetching data 2...');
-        // Your fetchData1 logic here
-        axios.get('http://localhost:8081/live').then(response => {
-    
-    console.log(response.data);  
-    let todisplay = response.data;
-    for(const iterator in todisplay)
-    {
-      if (todisplay[iterator].id.startsWith('Q')) {
-      // For 'Q' items
-      const queueId = todisplay[iterator].id;  // "Q0", "Q1", ...
-      const numericPart = queueId.slice(1);
-      const numberr = parseInt(numericPart, 10);
-      const numberOfElements = todisplay[iterator].numberOfElements;
-      if(numberr===this.queues.length-1)
-      this.queues[numberr].children[2].text(numberOfElements-this.number)
-      else
-      this.queues[numberr].children[2].text(numberOfElements)
-  
-      // Use the data for queues
-      // console.log(`Queue ID: ${queueId}, Number of Elements: ${numberOfElements}`);
-    } else if (todisplay[iterator].id.startsWith('M')) {
-      // For 'M' items
-      const machineId = todisplay[iterator].id;  // "M1", "M2", ...
-      const numericPart = machineId.slice(1);
-      const numberr = parseInt(numericPart, 10);
-      // const numberOfElements = todisplay[iterator].numberOfElements;
-      const colorr = todisplay[iterator].colour;
-      const red = parseInt(colorr[0], 10);
-      const green = parseInt(colorr[1], 10);
-      const blue = parseInt(colorr[2], 10);
-  
-      // Create an RGB color string
-      const color = `rgb(${red}, ${green}, ${blue})`;
-      this.machines[numberr].children[0].fill('#008000');
-  
-      this.machines[numberr].children[0].fill(color);
-  
-      
-  
-      // Use the data for machines
-      // console.log(`Machine ID: ${machineId}, Number of Elements: ${numberOfElements}`);
-    }
-  
-  
-    }   
-    
-    // Assume 'jsonResponse' is the JSON response you received
-  
-  // Filter and process the data
-  
-  
-      })
-      .catch(error => {
-        console.error('There was an error!', error);
-        
-      })
-        // console.log('Fetching data...');
-      },
-  
-  
-  
-  
-      replay(){
-        axios.get('http://localhost:8081/replay');
-        this.startFetchData2();
-      },
-  
       connect(){
-      
-     
-       if(this.tr.nodes().length==2){
-  
-        
-       
-      
-        // console.log('lolo')
-        // console.log(this.tr.nodes()[1].x());
-  
-        const line = new Konva.Line({
-            points: [this.tr.nodes()[1].x()+100,this.tr.nodes()[1].y()+50,
-             this.tr.nodes()[0].x(), this.tr.nodes()[0].y()],
-            stroke: 'yellow',
-            strokeWidth: 4,
-           
-          });
-          // console.log('55555')
-          // console.log(this.tr.nodes()[0].children[0])
-  
-  
-          // Add the line to the layer
-          this.layer.add(line);
-          this.linelist.push(line);
-  
-          // Redraw the layer
-          this.layer.draw();
-          this.adjacency.push([this.tr.nodes()[0].children[0].id(),this.tr.nodes()[1].children[0].id(),this.gainamount]);
+        if(this.selectedNodes.length==1){
+          const customedge={
+          source: this.selectedNodes[0],
+          target: this.selectedNodes[0],
+          label: this.gainamount.toString(),
+        }
+            this.edges.push(customedge);
+            this.adjacency.push([this.selectedNodes[0],this.selectedNodes[0],this.gainamount]);
+            this.gainamount=1;
+            this.showmodal=false;
+            console.table(this.adjacency)
+            
+          }
+          
+          else if(this.selectedNodes.length==2){
+            const customedge={
+              source: this.selectedNodes[0],
+              target: this.selectedNodes[1],
+              label: this.gainamount.toString(),
+            }
+            this.edges.push(customedge);
+            console.log("edege"+this.edges[0].label);
+            this.adjacency.push([this.selectedNodes[0],this.selectedNodes[1] ,this.gainamount]);
           this.gainamount=1;
           this.showmodal=false;
-          // this.adjacency[this.tr.nodes()[0].children[0].id()][this.tr.nodes()[1].children[0].id()]=1;
           console.table(this.adjacency)
-          
-       
-  
-       
+      }
+      else{
+        alert("Please select one or two nodes to connect")
+        this.showmodal=false;
       }
       
         
       },
-  
-      addDoubleClickEvent(shape) {
-     shape.on('click', () => {
-       shape.moveToTop();
-       if(this.selected.length<2)
-       {
-        this.selected.push(shape);
-        this.tr.nodes(this.selected);
-       }
-       else{
-        this.selected=[];
-        this.selected.push(shape);
-        this.tr.nodes(this.selected);
-  
-       }
-       
-       this.stage.batchDraw();
-     });
-   
-     this.stage.on('click', (e) => {
-       if (e.target === this.stage) {
-         if (this.tr.nodes().length > 0) {
-           // Commented out to prevent calling onModify when clicking outside the shape
-           // this.onModify(this.tr.nodes()[0]);
-         }
-          this.tr.nodes([]);
-          this.selected=[];
-         return;
-       }
-     });},
     
      addMachine(){
-         var circle = new Konva.Group({
-            x: 150,
-            y: 150,
-            draggable: true,
-        });
-  
-        circle.add(new Konva.Circle({
-            radius: 75,
-            fill: '#008000',
-            draggable: true,
-            id:this.nodes.length,
-        }));
-        let x ="temp";
-        if(this.nodes.length===0)
-        {
-            x = "R";
-        }
-        else if(this.nodes.length===this.numnodes-1)
-      {
-        x = "C";
-      }
-      else{
-        x = "V"+this.nodes.length;
-      }
-        circle.add(new Konva.Text({
-            text:x,
-            fontSize: 50,
-            fontFamily: 'Calibri',
-            fill: 'black',
-            width: 100,
-            padding:5,
-            height:100,
-            align: 'center',
-            verticalAlign: 'middle',
-            offsetX:50,
-            offsetY:45,
-           
-        }));
-              
-  
-         this.addDoubleClickEvent(circle);
-         this.layer.add(circle);
-         this.nodes.push(circle);
-         this.layer.draw();
-  
-         
-  
+      const newNodeId = String.fromCharCode(97 + this.nodes.length % 26);
+    const customNode = {
+    nodeId: newNodeId,
+    name: newNodeId,
+  };
+    this.nodes.push(customNode);
+      console.log(this.nodes);
      },
   
      initialize()
@@ -496,18 +248,16 @@
   
      }
   
-  
-     },
-  
-  }
+    },
+     };
   </script>
   
   <style>
+ 
   p{
     font-family: Helvetica, Arial, sans-serif;
     /* font-size: small; */
   }
-  
   Button {
     /* margin: 10px 5px 1px 10px;
     margin-left: 1px;
@@ -534,6 +284,7 @@
     display: flex;
     justify-content: space-around;
     font-size: 20px;
+    widows: 100%;
   }
   
   .machine {
